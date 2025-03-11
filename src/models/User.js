d0535a284b569c23b4e17278d48808b9a55207df
Danyelle <dkobes15@gmail.com>
2025-03-10 21:08:24 -0500

    fixing spelling error in User.js

--- src/models/User.js
+++ src/models/User.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
         ref: 'Reaction',
     },
 ],
-    fiends : [{
+    friends : [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'USer',
     },
